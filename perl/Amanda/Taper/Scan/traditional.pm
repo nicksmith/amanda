e6fe8f00e910f8bcba0f226e54c406855600251f
Dustin J. Mitchell <dustin@zmanda.com>
2009-04-09 17:54:45 -0400

    Taperscan: WIP

---
+++ perl/Amanda/Taper/Scan/traditional.pm
@@ -0,0 +1,100 @@
+# Copyright (c) 2005-2008 Zmanda, Inc.  All Rights Reserved.
+#
+# This library is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License version 2.1 as
+# published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+# License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA.
+#
+# Contact information: Zmanda Inc., 465 S Mathlida Ave, Suite 300
+# Sunnyvale, CA 94086, USA, or: http://www.zmanda.com
+
+package Amanda::Taper::Scan::traditional;
+
+=head1 NAME
+
+Amanda::Taper::Scan::traditional
+
+=head1 SYNOPSIS
+
+This is an abstract base class for taperscan algorithms.
+
+  # open the configured taperscan algorithm
+  my $taperscan = Amanda::Taperscan->new();
+
+  my $result_cb = sub {
+    my ($err, $reservation, $label, $access_mode) = @_;
+    die $err if $err;
+    # write to $reservation->{'device'}, using label $label, and opening
+    # the device with $access_mode (one of $ACCESS_WRITE or $ACCESS_APPEND)
+    # ..
+  };
+  $taperscan->scan(result_cb => $result_cb);
+
+=head1 OVERVIEW
+
+C<Amanda::Taper::Scan> subclasses represent algorithms used by
+C<Amanda::Taper::Scribe> (see L<Amanda::Taper::Scribe>) to scan for and select
+volumes for writing.
+
+Call C<Amanda::Taperscan->new()> to create a new taperscan algorithm of the
+class specified by the user in the Amanda configuration file.
+
+Subclasses must implement a single method: C<scan>.  It takes only one named
+parameter:
+
+  $taperscan->scan(result_cb => $my_result_cb);
+
+This callback takes the following parameters:
+
+  $error        an error message, or undef on success
+  $reservation  Amanda::Changer::Reservation object
+  $label        label to apply to the volume
+  $access_mode  access mode with which to start the volume
+
+The error message can be a simple string or an C<Amanda::Changer::Error> object
+(see L<Amanda::Changer>).  The C<$label> and C<$access_mode> specify parameters
+for starting the device contained in C<$reservation>.
+
+=cut
+
+use strict;
+use warnings;
+
+sub new {
+    my $class = shift;
+    my $algo = "traditional";
+
+    my $pkgname = "Amanda::Taper::Scan::$algo";
+
+    my $filename = $pkgname;
+    $filename =~ s|::|/|g;
+    $filename .= '.pm';
+    if (!exists $INC{$filename}) {
+	eval "use $pkgname;";
+	if ($@) {
+	    # handle compile errors
+	    die($@) if (exists $INC{$filename});
+	    die("No such taperscan algorithm '$algo'");
+	}
+    }
+
+    return $pkgname->new();
+}
+
+sub scan {
+    my $self = shift;
+    my ($result_cb) = @_;
+
+    $result_cb->("not implemented");
+}
+
+1;
+
